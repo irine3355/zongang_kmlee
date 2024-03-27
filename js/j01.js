7545f03f6b6d4788bf188de6eb5c8b910fb3e7a0
irine3355 <irine3355@gmail.com>
2024-03-27 16:57:10 +0900

    3_27_02

--- js/j01.js
+++ js/j01.js
@@ -1,3 +1,18 @@
 function add(...params) {
     console.log(add=0, add+=1)
 }
+
+
+//fgrt
+
+class Ellipse {
+    constructor (a, b) {
+        this. a=a;
+        this. b= b;
+
+    }
+    area () {
+        console.log ("면적:", this.a * this.b * Math. PI);
+    }
+}
+
