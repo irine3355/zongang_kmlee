8c5738a8058ff49bb21d8fc6bb6e30bc50239db1
irine3355 <irine3355@gmail.com>
2024-04-15 06:46:15 +0900

    4-15

---
+++ 3_응용SW기초기술활용/source/day01_2/src/exam01/ex14_1.java
@@ -0,0 +1,16 @@
+package exam01;
+
+public class ex14_1 {
+    public static void main(String[] args) {
+    int sum = add(10,20);
+        System.out.println(sum);
+    }
+
+        // add() 함수
+        static int add(int n1, int n2) {
+            int sum = n1 + n2;
+            return sum; // 결과 값 반환
+        }
+}
+
+
---
+++ 3_응용SW기초기술활용/source/day01_2/src/exam01/ex14_2.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class ex14_2 {
+
+
+
+
+        public static void main(String[] args) {
+            Person personLee = new Person(); // Person()  - 생성자
+        }
+
+
+}
