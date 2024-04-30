d9ba501de184400b9720449445d6ba341c2ea662
irine3355 <irine3355@gmail.com>
2024-04-30 17:53:40 +0900

    4-30

--- 4_프로그래밍_언어활용/source/day12/src/exam03_q5/AbstractCoffeeShop.java
+++ 4_프로그래밍_언어활용/source/day12/src/exam03_q5/AbstractCoffeeShop.java
@@ -4,21 +4,21 @@ import java.util.Map;
 
 public abstract class AbstractCoffeeShop implements CoffeeShop {
 
-    private String name; // 커피샵 이름
+    private String sName; // 커피샵 이름
     private int totalSalePrice;
     private Map<String, Integer> menus;
     private Person person;
 
     public AbstractCoffeeShop(String name, Map<String, Integer> menus) {
-        this.name = name;
+        this.sName = name;
         this.menus = menus;
 
     }
 
     @Override
-    public CoffeeShop enter(Person person) {
+    public CoffeeShop pEnter(Person person) {
         this.person = person;
-        System.out.printf("%s님이 %s에 입장하였습니다. %n", person.getName(), name);
+        System.out.printf("%s님이 %s에 입장하였습니다. %n", person.getName(), sName);
 
         return this;
     }
@@ -38,29 +38,29 @@ public abstract class AbstractCoffeeShop implements CoffeeShop {
         int money = person.getMoney();
 
         if (money < price) {
-            throw new RuntimeException(menu + "%s를 구입하기에 부족한 금액입니다.");
+            throw new RuntimeException(menu + "를 구입하기에 부족한 금액입니다.");
         }
 
         totalSalePrice += price;
         money -= price;
         person.setMoney(money);
 
-        System.out.printf("%s님이 %s에서 %s를 %d원에 주문했습니다. %n", person.getName(), name, price);
+        System.out.printf("%s님이 %s에서 %s를 %d원에 주문했습니다.%n", person.getName(), sName, menu, price);
 
         return this;
     }
 
     public void exit() {
-        System.out.printf("%s님이 %s애소 퇴장하였습니다. %n", person.getName(), name);
+        System.out.printf("%s님이 %s애소 퇴장하였습니다. %n", person.getName(), sName);
         person = null;
     }
 
-    public int getTotalSalePrice(){
+    public int getTotalSalePrice() {
         return totalSalePrice;
     }
 
-    public String getName(){
-        return name;
+    public String getName() {
+        return sName;
     }
 
 }
