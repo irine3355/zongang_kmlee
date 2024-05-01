cce234e900f9b8476f7d3a8685c0c673a0a556f6
irine3355 <irine3355@gmail.com>
2024-05-01 17:52:50 +0900

    4-30

---
+++ 4_프로그래밍_언어활용/source/day12/src/exam03_q5/exam03_q5_2/CoffeeShop.java
@@ -0,0 +1,9 @@
+package exam03_q5.exam03_q5_2;
+
+public interface CoffeeShop {
+    CoffeeShop pEnter(Person person);//Person 클래스 , person 메소드의 매개변수 이름
+//메소드는 CoffeeShop 인터페이스에 선언된 메소드입니다. 이 메소드는 Person 객체를 매개변수로 받아들이고, CoffeeShop을 반환합니다.
+    CoffeeShop order();
+
+
+}
