ac38be3d58b36e0feae7b58a170413a8788d00bf
irine3355 <irine3355@gmail.com>
2024-05-30 12:41:14 +0900

    Merge remote-tracking branch 'origin/master'

--- 8_실무_프로젝트p_2/socketserver/src/main/java/org/choongang/Ex02.java
+++ 8_실무_프로젝트p_2/socketserver/src/main/java/org/choongang/Ex02.java
@@ -4,7 +4,7 @@ import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Ex01 {
+public class Ex02 {
     public static void main(String[] args) throws Exception {
         Socket socket = new Socket("127.0.0.1", 9999);
         Scanner sc = new Scanner(System.in);
--- 8_실무_프로젝트p_2/socketserver/src/main/java/org/choongang/Server.java
+++ 8_실무_프로젝트p_2/socketserver/src/main/java/org/choongang/Server.java
@@ -1,11 +1,7 @@
 
 package org.choongang;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,60 +12,59 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class Server {
-
+    private ServerSocket serverSocket;
     private ExecutorService threadPool;
-    private ObjectMapper om;
 
     public Server() {
-        threadPool = new ThreadPoolExecutor(2, 100, 120L, TimeUnit.SECONDS, new SynchronousQueue<>(){});
+        try {
+            serverSocket = new ServerSocket(9999);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        om = new ObjectMapper();
-        om.registerModule(new JavaTimeModule()); // java.time 패키지 대응
+        threadPool = new ThreadPoolExecutor(2, 100, 120L, TimeUnit.SECONDS, new SynchronousQueue<>(){});
     }
 
     public void start() {
-        try {
-            ServerSocket server = new ServerSocket(9999);
 
-            while(true) {
-                Socket socket = server.accept();
+        while(true) {
+            try {
+                Socket socket = serverSocket.accept();
 
-                SocketHandler socketHandler = new SocketHandler(socket);
+                SocketHandler handler = new SocketHandler(socket);
 
                 // 수신 처리
-                socketHandler.inputHandler((data) -> {
-
+                handler.input(data -> {
+                    System.out.println(data);
                 });
 
-                // 전송 처리
-                socketHandler.outputHandler();
-
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
     class SocketHandler {
         private Socket socket;
-        private Object data;
 
         public SocketHandler(Socket socket) {
             this.socket = socket;
-
         }
 
-        public void setData(Object data) {
-            this.data = data;
-        }
-
-        public void inputHandler(Consumer<String> handler) {
+        // 수신 처리
+        public void input(Consumer<String> handler) {
             threadPool.execute(() -> {
-                try (DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+                try(DataInputStream dis = new DataInputStream(socket.getInputStream())) {
 
-                    String data = dis.readUTF();
-                    handler.accept(data);
+                    while(true) {
+                        if (socket == null || handler == null) {
+                            Thread.currentThread().yield();
+                            break;
+                        }
+
+                        String data = dis.readUTF();
+                        handler.accept(data);
+                    }
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -77,24 +72,10 @@ public class Server {
             });
         }
 
-        public void outputHandler() {
+        // 전송 처리
+        public void output() {
             threadPool.execute(() -> {
-                while(true) {
-                    if (data == null) {
-                        Thread.currentThread().yield();
-                        continue;
-                    }
 
-                    try (DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
-                        // 자바 객체 data -> JSON 문자열 변환
-                        String json = om.writeValueAsString(data);
-                        dos.writeUTF(json); // 데이터 전송
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    data = null; // 전송 후 데이터 비우기
-                }
             });
         }
     }
